af44ecc170b958413a124b9909ecb609c9e04a44
SharleneYu <sharleneyu@hotmail.com>
2023-09-05 15:05:07 +0800

    完成P31九News5/6/7/8、 P35Menu十二2/7前台頁面顯示及設定

--- controller/News.php
+++ controller/News.php
@@ -17,8 +17,63 @@ class News extends DB{
         return $this->view('./view/backend/news.php', $view);
     }
 
+    function show(){
+        //將sh=1的資料撈出來, 存在$rows。加上條件只能5筆
+        $rows = $this->all(['sh'=>1], " LIMIT 5 ");
+
+        foreach($rows as $row){
+            echo "<li>";    
+            echo mb_substr($row['text'], 0, 20);
+            echo "<span class='all' style='display:none'>";
+            echo $row['text'];
+            echo "</span>";
+            echo "</li>";   
+        }
+        return;
+
+    }
+
+    function more(){
+        //若最新消息有超過5則，
+        // if($this->count(['sh'=>1] >5)){
+        if($this->count(['sh'=>1])>5){
+            echo "<a href='?do=news' style='float:right'>";
+            echo "More...";
+            echo "</a>";
+        }
+    }
+
+    //改自$News->show()
+    function moreNews(){
+        //將sh=1的資料撈出來, 存在$rows。加上條件只能5筆
+        $rows = $this->paginate(5, ['sh'=>1]);
+        $start=$this->links['start']+1;
+
+        echo "<ol start='$start'>";
+
+        foreach($rows as $row){
+            echo "<li>";    
+            echo mb_substr($row['text'], 0, 20);
+            echo "<span class='all' style='display:none'>";
+            echo $row['text'];
+            echo "</span>";
+            echo "</li>";   
+        }
+        echo "</ol>";
+        echo "<div class='cent'>";
+        echo $this->links();
+        echo "</div>";
+
+    }
+      
+      
+
+
+
+
 
 }
 
 
 ?>
+
--- index.php
+++ index.php
@@ -29,6 +29,34 @@
 				<div id="menuput" class="dbor">
 					<!--主選單放此-->
 					<span class="t botli">主選單區</span>
+
+					<?php
+						$rows= $Menu->show();
+
+						foreach($rows as $row){
+
+							echo "<div class='mainmu'>";
+								echo "<a href='{$row['href']}'>";
+									echo $row['text'];
+								echo "</a>";
+							
+							if(isset($row['subs'])){
+								echo "<div class='mw'>";
+
+								foreach($row['subs'] as $sub){
+									echo "<div class='mainmu2'>";
+									echo "<a href='{$sub['href']}'>";
+										echo $sub['text'];
+									echo "</a>";
+									echo "</div>";
+								}
+								echo "</div>";
+							}
+							echo "</div>";
+						}
+						
+					?>
+
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
 					<span class="t">進站總人數 :<?=$Total->show();?></span>
@@ -90,3 +118,6 @@
 </body>
 
 </html>
+
+
+
